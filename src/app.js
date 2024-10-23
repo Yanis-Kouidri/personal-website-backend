0ae62fc021d3cd3f8fba858477e2ec9a53acd89e
Yanis Kouidri <yanis.kouidri@gmail.com>
2024-10-23 21:55:13 +0200

    configure pnp with yarn

---
+++ src/app.js
@@ -0,0 +1,9 @@
+import express from 'express'
+
+const app = express();
+
+app.use((req, res) => {
+   res.json({ message: 'Votre requête a bien été reçue !' }); 
+});
+
+export default app
---
+++ src/server.js
@@ -0,0 +1,8 @@
+import http from "http"
+import app from "./app.js"
+
+
+app.set('port', process.env.PORT || 3000);
+const server = http.createServer(app);
+
+server.listen(process.env.PORT || 3000);
