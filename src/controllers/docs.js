5d0285e62ce7e0b0f864baba70189af67a8cc647
Yanis Kouidri <yanis.kouidri@gmail.com>
2025-01-05 11:39:41 +0100

    typo and indentation

--- src/controllers/docs.js
+++ src/controllers/docs.js
@@ -1,23 +1,24 @@
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-import fs from 'fs';
-import path from 'path';
+import { fileURLToPath } from 'url'
+import { dirname } from 'path'
+import fs from 'fs'
+import path from 'path'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 export const getAllDocs = (req, res) => {
-  const docsDir = path.join(__dirname, '../../data/docs');
+  const docsDir = path.join(__dirname, '../../data/docs')
 
   fs.readdir(docsDir, (err, files) => {
     if (err) {
-      return res.status(500).json({ message: 'Erreur lors de la lecture du dossier' });
+      return res
+        .status(500)
+        .json({ message: 'Erreur lors de la lecture du dossier' })
     }
-    res.json(files);
-  });
-};
+    res.json(files)
+  })
+}
 
-
- export const addOneDoc = (req, res) => {
-    res.json({ message: 'Vous voulez ajouter une doc' }); 
- }
+export const addOneDoc = (req, res) => {
+  res.json({ message: 'Vous voulez ajouter une doc' })
+}
