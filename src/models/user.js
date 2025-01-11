6ea95e2e6b082b160569a29911de23dff2171480
Yanis Kouidri <yanis.kouidri@gmail.com>
2025-01-11 15:53:51 +0100

    add moogoose schema for user

---
+++ src/models/user.js
@@ -0,0 +1,9 @@
+import mongoose from 'mongoose';
+
+export const userSchema = mongoose.Schema({
+    username: {type: String, required: true, unique: true},
+    password: {type: String, required: true}
+})
+
+
+
