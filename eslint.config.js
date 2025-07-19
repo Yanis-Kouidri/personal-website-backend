e34ad86dc9eaf67e526122d68ec3101895213fcc
Yanis Kouidri <yanis.kouidri@gmail.com>
2025-07-19 17:59:10 +0200

    add better linting

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,18 @@
+import js from '@eslint/js'
 import globals from 'globals'
-import pluginJs from '@eslint/js'
+import { defineConfig, globalIgnores } from 'eslint/config'
+import nodePlugin from 'eslint-plugin-n'
 
-export default [
-  { languageOptions: { globals: globals.node } },
-  pluginJs.configs.recommended,
-]
+export default defineConfig([
+  globalIgnores(['dist', '.yarn', '.pnp.*', 'eslint.config.js']),
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+    languageOptions: {
+      globals: globals.node,
+      sourceType: 'module',
+    },
+  },
+  nodePlugin.configs['flat/recommended'],
+])
