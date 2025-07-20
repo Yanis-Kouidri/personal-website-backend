be1a3d8d6a474ba16e9fb4e1445ebfdc7c03a4c2
Yanis Kouidri <yanis.kouidri@gmail.com>
2025-07-20 21:51:55 +0200

    add: integrate eslint-plugin-security

--- eslint.config.js
+++ eslint.config.js
@@ -6,6 +6,7 @@ import nodePlugin from 'eslint-plugin-n'
 import importPlugin from 'eslint-plugin-import'
 import pluginPromise from 'eslint-plugin-promise'
 import eslintConfigPrettier from 'eslint-config-prettier/flat'
+import pluginSecurity from 'eslint-plugin-security'
 
 export default defineConfig([
   globalIgnores(['dist', '.yarn', '.pnp.*', 'eslint.config.js']),
@@ -23,4 +24,5 @@ export default defineConfig([
   importPlugin.flatConfigs.recommended,
   pluginPromise.configs['flat/recommended'],
   eslintConfigPrettier,
+  pluginSecurity.configs.recommended,
 ])
