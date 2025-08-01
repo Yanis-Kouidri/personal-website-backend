011bbd289acd4ec44419f0c1ea8d1f3f7774a766
Yanis Kouidri <yanis.kouidri@gmail.com>
2025-08-01 22:30:04 +0200

    add: begginig of test

--- tests/controllers/documentation.test.js
+++ tests/controllers/documentation.test.js
@@ -1,19 +1,25 @@
+import fs from 'node:fs'
+
 import { getAllDocumentation } from '../../src/controllers/documentation'
 
-describe('authentication middleware', () => {
-  let request, response
+//jest.mock('node:fs')
+
+describe('test getAllDocumentation controller', () => {
+  let mockRequest, mockResponse
 
   beforeEach(() => {
-    response = {
+    mockResponse = {
       status: jest.fn().mockReturnThis(),
-      json: jest.fn(),
+      json: jest.fn().mockReturnThis(),
     }
   })
 
   it('should return a json file', () => {
-    getAllDocumentation(request, response)
+    //fs.existsSync.mockResolvedValue(true)
 
-    expect(response.status).not.toHaveBeenCalled() // Because express automaticly set 200 by default so it can't be test here because it mocked
-    expect(response.json).toHaveBeenCalled()
+    getAllDocumentation(mockRequest, mockResponse)
+
+    expect(mockResponse.status).not.toHaveBeenCalled() // Because express automaticly set 200 by default so it can't be test here because it mocked
+    expect(mockResponse.json).toHaveBeenCalledOnce()
   })
 })
