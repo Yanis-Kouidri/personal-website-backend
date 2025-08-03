e5cb062eb43eab5c37f2f206fd9dfde0b22f07a7
Yanis Kouidri <yanis.kouidri@gmail.com>
2025-08-03 10:07:59 +0200

    add: test

--- tests/jest-setup.js
+++ tests/jest-setup.js
@@ -1,3 +1,4 @@
 // Mock console.error to suppress logs during Jest tests
 jest.spyOn(console, 'error').mockImplementation(() => {})
 jest.spyOn(console, 'log').mockImplementation(() => {})
+jest.spyOn(console, 'warn').mockImplementation(() => {})
