6a3bb92f4614d4e4839ab549b536a8dc7decce0b
Yanis Kouidri <yanis.kouidri@gmail.com>
2026-01-02 23:00:35 +0100

    chore: allows logs in vitest

--- tests/vitest.setup.js
+++ tests/vitest.setup.js
@@ -1,6 +1,6 @@
 beforeAll(() => {
   // Mock console methods to suppress logs during tests
-  vi.spyOn(console, 'error').mockImplementation(() => {})
-  vi.spyOn(console, 'log').mockImplementation(() => {})
-  vi.spyOn(console, 'warn').mockImplementation(() => {})
+  //vi.spyOn(console, 'error').mockImplementation(() => {})
+  //vi.spyOn(console, 'log').mockImplementation(() => {})
+  //vi.spyOn(console, 'warn').mockImplementation(() => {})
 })
