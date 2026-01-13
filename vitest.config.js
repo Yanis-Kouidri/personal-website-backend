a43182ea24537c77e1741343e1c715e2a0b17286
Yanis Kouidri <yanis.kouidri@ik.me>
2026-01-13 08:30:26 +0100

    ops: configure coverage for sonarqube

--- vitest.config.js
+++ vitest.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     setupFiles: ['./tests/vitest.setup.js'],
     coverage: {
       provider: 'v8',
-      reporter: ['text', 'json', 'html'],
+      reporter: ['text', 'json', 'html', 'lcov'],
     },
   },
 })
